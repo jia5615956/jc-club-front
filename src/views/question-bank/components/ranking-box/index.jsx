e7b6b11f8a2488dc9c683deebee92979fd2f65a2
秋水浮尘 <18510549872@163.comm>
2023-10-26 00:46:49 +0800

    feat: 调整答案样式

--- src/views/question-bank/components/ranking-box/index.jsx
+++ src/views/question-bank/components/ranking-box/index.jsx
@@ -3,6 +3,7 @@ import { Popover, Spin } from 'antd'
 import { debounce } from '@utils'
 import { imgObject, RankingTypeText, RankingTypeBtnText } from '../../constant'
 import './index.less'
+import { message } from 'antd'
 
 const rankingBackImg = {
   0: imgObject.ranking1Img,
@@ -17,7 +18,8 @@ export default function RankingBox(props) {
       props.onHandleRanking && props.onHandleRanking(index)
     })
   const onJump = debounce(() => {
-    props.onHandleJump && props.onHandleJump()
+    // props.onHandleJump && props.onHandleJump()
+    message.info('敬请期待')
   })
   const tabList = [
     {
